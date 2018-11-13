7abd820b5470be311eb821f169aff8adcdf9027f
Ricky Bruner <ricky.bruner@gmail.com>
2018-11-13 10:47:05 -0600

    updated gitignore, models, and sql file

---
+++ BangazonWorkforce/BangazonWorkforce/Models/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforce.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int? PaymentTypeId { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public List<Product> Products { get; set; } = new List<Product>();
+    }
+}
