04ed47fc791735acf8ea5495565833015a489213
Figamus <fig74656@yahoo.com>
2018-11-14 15:12:54 -0600

    checking out branch

---
+++ BangazonWorkforce/BangazonWorkforce/Models/ViewModels/DepartmenIndexViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BangazonWorkforce.Models
+{
+    public class DepartmentIndexViewModel
+    {
+        public Department Department { get; set; }
+        public List<Employee> AllEmployees { get; set; }
+        public List<SelectListItem> AllDepartmentOptions
+        {
+            get
+            {
+                if (AllDepartments == null)
+                {
+                    return null;
+                }
+
+                return AllDepartments
+                        .Select((d) => new SelectListItem(d.Name, d.Id.ToString()))
+                        .ToList();
+            }
+        }
+    }
+}
