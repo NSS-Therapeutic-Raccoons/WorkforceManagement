369c427a51903d9c875f53eae97a2cf87bb8d468
Ricky Bruner <ricky.bruner@gmail.com>
2018-11-19 17:04:12 -0600

    added tp controller, views, models, and index functionality

---
+++ BangazonWorkforce/BangazonWorkforce/Models/ViewModels/TrainingProgramDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforce.Models.ViewModels
+{
+    public class TrainingProgramDetailsViewModel
+    {
+        
+        
+
+    }
+
+}
---
+++ BangazonWorkforce/BangazonWorkforce/Models/ViewModels/TrainingProgramIndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonWorkforce.Models.ViewModels
+{
+    public class TrainingProgramIndexViewModel
+    {
+
+        public List<TrainingProgram> TrainingPrograms { get; set; }
+
+    }
+}
